747a48c956b83198209667a8a0bd9b630e788d33
ruslstepanson753 <ruslstepanson753@gmail.com>
2025-04-02 14:36:33 +0300

    7. MVC header works

--- src/main/java/com/javarush/stepanov/controllers/MainController.java
+++ src/main/java/com/javarush/stepanov/controllers/MainController.java
@@ -6,9 +6,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/home"})
     public String home(Model model) {
         model.addAttribute("header", "Главная страница");
-        return "home";
+        return "home"; // Возвращает шаблон home.html из папки templates
     }
+
+    @GetMapping(value = "/run")
+    public String run(Model model) {
+        model.addAttribute("header", "Главная страница");
+        return "run"; // Возвращает шаблон home.html из папки templates
+    }
+
+    @GetMapping(value = "/settings")
+    public String settings(Model model) {
+        model.addAttribute("header", "Главная страница");
+        return "settings"; // Возвращает шаблон home.html из папки templates
+    }
+
+    @GetMapping(value = "/statistic")
+    public String statistic(Model model) {
+        model.addAttribute("header", "Главная страница");
+        return "statistic"; // Возвращает шаблон home.html из папки templates
+    }
+
+    @GetMapping(value = "/hall-of-fame")
+    public String hallOfFame(Model model) {
+        model.addAttribute("header", "Главная страница");
+        return "hall-of-fame"; // Возвращает шаблон home.html из папки templates
+    }
+
+    @GetMapping(value = "/exit")
+    public String exit(Model model) {
+        model.addAttribute("header", "Главная страница");
+        return "exit"; // Возвращает шаблон home.html из папки templates
+    }
+
 }
