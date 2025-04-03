90b67ff1ba4af6e9c9f3b6161f914fb8f37dd5ee
ruslstepanson753 <ruslstepanson753@gmail.com>
2025-04-03 04:03:45 +0300

    9. MVC run question answer full

--- src/main/java/com/javarush/stepanov/controllers/RunController.java
+++ src/main/java/com/javarush/stepanov/controllers/RunController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -19,12 +20,25 @@ public class RunController {
 
     @GetMapping("/run")
     public String run(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        Long userId = CookieHelp.getUserIdFromCookies(cookies);
-        Map.Entry<String,String> questionAnswerSet= userService.getQuestionAnswerSetById(userId);
+        Long userId = getUserId(request);
+        Map.Entry<String,String> questionAnswerSet= userService.getQuestionAnswerEntryById(userId);
         request.setAttribute("question", questionAnswerSet.getKey());
         request.setAttribute("answer", questionAnswerSet.getValue());
         return "run";
+    }
+
+    @PostMapping("/run")
+    public String runPost(HttpServletRequest request) {
+        Long userId = getUserId(request);
+        String qoodAnswer = request.getParameter("good-answer");
+        userService.answerProcessing(userId,qoodAnswer);
+        return "redirect:/run";
+    }
+
+    private Long getUserId(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        Long userId = CookieHelp.getUserIdFromCookies(cookies);
+        return userId;
     }
 
 }
--- src/main/java/com/javarush/stepanov/service/UserService.java
+++ src/main/java/com/javarush/stepanov/service/UserService.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,12 +20,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final QuestionService questionService;
 
-    public UserDto getUserById(Long id) {
-        Optional<User> user = userRepository.findById(id);
-        UserDto userDto = UserDto.fromEntity(user.get());
-        return userDto;
-    }
-
     @Override
     public String toString() {
         return "DemoService{" +
@@ -32,9 +27,8 @@ public class UserService {
                 '}';
     }
 
-    public Map.Entry<String, String> getQuestionAnswerSetById(Long id) {
-        Optional<User> userOptional = userRepository.findById(id);
-        User user = userOptional.orElseThrow();
+    public Map.Entry<String, String> getQuestionAnswerEntryById(Long id) {
+        User user = getUser(id);
         String topic = user.getTopic();
         Map<String, Integer> questionPositions = user.getQuestionPositions();
         Map<String, List<String>> questions = user.getQuestions();
@@ -45,5 +39,49 @@ public class UserService {
         String answer = questionService.getAnswer(question);
         Map.Entry<String, String> result = Map.entry(question, answer);
         return result;
+    }
+
+    public void answerProcessing(Long userId, String qoodAnswer) {
+        User user = getUser(userId);
+        if(qoodAnswer.equals("true")){
+            doGoodAnswerChanges(user);
+        }else {
+            doBadAnswerChanges(user);
+        }
+        userRepository.save(user);
+    }
+
+    private void doBadAnswerChanges(User user) {
+        String topic = user.getTopic();
+        Map<String, Integer> questionPositions = user.getQuestionPositions();
+        Map<String, List<String>> questions = user.getQuestions();
+
+        List<String> questionList = questions.get(topic);
+        Integer questionPosition = questionPositions.get(topic);
+        Integer newQuestionPosition;
+        if (questionPosition < questionList.size()){
+            newQuestionPosition = questionPosition + 1;
+        } else {
+            newQuestionPosition = 0;
+        }
+        questionPositions.put(topic, newQuestionPosition);
+    }
+
+    private void doGoodAnswerChanges(User user) {
+        String topic = user.getTopic();
+        Map<String, Integer> questionPositions = user.getQuestionPositions();
+        Map<String, List<String>> questions = user.getQuestions();
+
+        List<String> questionList = questions.get(topic);
+        int questionPosition = questionPositions.get(topic).intValue();
+        if (questionList.size()>0){
+            questionList.remove(questionPosition);
+        }
+    }
+
+    private User getUser(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        User user = userOptional.orElseThrow();
+        return user;
     }
 }
