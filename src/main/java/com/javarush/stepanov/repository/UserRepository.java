0355e5b11086ad8f761a8763d9adfa4699d1ac91
user <stepanovruslan@mail.ru>
2025-04-07 15:31:43 +0300

    9 docker railway

--- src/main/java/com/javarush/stepanov/repository/UserRepository.java
+++ src/main/java/com/javarush/stepanov/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.javarush.stepanov.repository;
 
 import com.javarush.stepanov.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
+@Repository
+@JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Transactional
